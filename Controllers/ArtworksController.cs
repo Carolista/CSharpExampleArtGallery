dfa3b916908995a63356771e388160dc88202b85
Carolista <cjones.renaissancewoman@gmail.com>
2024-05-28 22:44:45 -0400

    added controllers and methods for artworks pages

---
+++ Controllers/ArtworksController.cs
@@ -0,0 +1,98 @@
+﻿using System.Text;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CSharpExampleArtGallery;
+
+[Route("/artworks")]
+public class ArtworksController : Controller
+{
+    // TEST DATA - simulate database
+    private static int nextId = 6;
+    private static readonly Dictionary<int, string> artworks = new() {
+        { 1, "Girl with a Pearl Earring" },
+        { 2, "Mona Lisa" },
+        { 3, "The Birth of Venus" },
+        { 4, "The Persistence of Memory" },
+        { 5, "The Starry Night" },
+    };
+
+    // Corresponds to http://localhost:50xx/artworks
+    [HttpGet]
+    [Route("")] // This could also be left out entirely as the base route
+    public IActionResult RenderArtworksPage()
+    {
+        StringBuilder artworksList = new();
+        foreach (int artworkId in artworks.Keys)
+        {
+            string artwork = artworks[artworkId];
+            artworksList
+                .Append("<li><a href='/artworks/details/")
+                .Append(artworkId)
+                .Append("'>")
+                .Append(artwork)
+                .Append("</a></li>");
+        }
+        string html = "<html>" +
+            "<body>" +
+            "<h2>ARTWORKS</h2>" +
+            "<ul>" +
+            artworksList +
+            "</ul>" +
+            "<p>Click <a href='/artworks/add'>here</a> to add another artwork.</p>" +
+            "</body>" +
+            "</html>";
+        return Content(html, "text/html");
+    }
+
+    // Corresponds to http://localhost:50xx/artworks/add for GET
+    [HttpGet]
+    [Route("add")]
+    public IActionResult RenderAddArtworkForm()
+    {
+        string html = "<html>" +
+            "<body>" +
+            "<form action='/artworks/add' method='POST'>" +
+            "<p>Enter the name of a new work of art:</p>" +
+            "<input type='text' name='artwork' />" +
+            "<button type='submit'>Submit</button>" +
+            "</form>" +
+            "</body>" +
+            "</html>";
+        return Content(html, "text/html");
+    }
+
+    // Corresponds to http://localhost:50xx/artworks/add for POST
+    [HttpPost]
+    [Route("add")]
+    public IActionResult ProcessAddArtworkForm(string artwork)
+    {
+        artworks.Add(nextId, artwork);
+        nextId++;
+        string html = "<html>" +
+            "<body>" +
+            "<h3>ARTWORK ADDED</h3>" +
+            "<p>You have successfully added " + artwork + " to the collection.</p>" +
+            "<p><a href='/artworks/add'>Add another artwork</a> or <a href='/artworks'>view the updated list</a> of artworks.</p>" +
+            "</body>" +
+            "</html>";
+        return Content(html, "text/html");
+    }
+
+    // Corresponds to http://localhost:50xx/artworks/details/42
+    [HttpGet]
+    [Route("details/{artworkId}")]
+    public IActionResult RenderArtworkDetailsPage(int artworkId)
+    {
+        Console.WriteLine(artworkId);
+        string html = "<html>" +
+            "<body>" +
+            "<h3>Artwork</h3>" +
+            "<p><b>ID:</b> " + artworkId + "</p>" +
+            "<p><b>Name:</b> " + artworks[artworkId] + "</p>" +
+            "<hr />" +
+            "<p>Return to the full <a href='/artworks'>list</a> of artworks.</p>" +
+            "</body>" +
+            "</html>";
+        return Content(html, "text/html");
+    }
+}
---
+++ Controllers/GalleryController.cs
@@ -0,0 +1,106 @@
+﻿using System.Text;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CSharpExampleArtGallery;
+
+// If route is unspecified here, it will be /gallery 
+// because this is the GalleryController
+public class GalleryController : Controller
+{
+
+    // GET: /gallery/ (which is the same as /gallery/index.html)
+    public IActionResult Index() 
+    {
+        string html = "<h2>Midtown Art Gallery</h2>" +
+            "<p>Welcome! View our <a href='/artworks'>collection</a> of fine art.</p>";
+        return Content(html, "text/html");
+    }
+
+    /* ARTWORKS */
+
+    // TEST DATA - simulate database
+    // private static int nextId = 6;
+    // private static readonly Dictionary<int, string> artworks = new() {
+    //     { 1, "Girl with a Pearl Earring" },
+    //     { 2, "Mona Lisa" },
+    //     { 3, "The Birth of Venus" },
+    //     { 4, "The Persistence of Memory" },
+    //     { 5, "The Starry Night" },
+    // };
+
+    // // GET: /gallery/artworks
+    // // Corresponds to http://localhost:50xx/gallery/artworks
+    // public IActionResult Artworks()
+    // {
+    //     StringBuilder artworksList = new();
+    //     foreach (int artworkId in artworks.Keys)
+    //     {
+    //         string artwork = artworks[artworkId];
+    //         artworksList
+    //             .Append("<li><a href='details/")
+    //             .Append(artworkId)
+    //             .Append("'>")
+    //             .Append(artwork)
+    //             .Append("</a></li>");
+    //     }
+    //     string html = "<html>" +
+    //         "<body>" +
+    //         "<h2>ARTWORKS</h2>" +
+    //         "<ul>" +
+    //         artworksList +
+    //         "</ul>" +
+    //         "<p>Click <a href='add'>here</a> to add another artwork.</p>" +
+    //         "</body>" +
+    //         "</html>";
+    //     return Content(html, "text/html");
+    // }
+
+    // // GET: /gallery/add
+    // // Corresponds to http://localhost:50xx/gallery/add
+    // public IActionResult Add()
+    // {
+    //     string html = "<html>" +
+    //         "<body>" +
+    //         "<form action='results' method='GET'>" +
+    //         "<p>Enter the name of a new work of art:</p>" +
+    //         "<input type='text' name='artwork' />" +
+    //         "<button type='submit'>Submit</button>" +
+    //         "</form>" +
+    //         "</body>" +
+    //         "</html>";
+    //     return Content(html, "text/html");
+    // }
+
+    // // GET: /gallery/results
+    // // Corresponds to http://localhost:50xx/gallery/results?artwork=Some+Art
+    // public IActionResult Results(string artwork)
+    // {
+    //     artworks.Add(nextId, artwork);
+    //     nextId++;
+    //     string html = "<html>" +
+    //         "<body>" +
+    //         "<h3>ARTWORK ADDED</h3>" +
+    //         "<p>You have successfully added " + artwork + " to the collection.</p>" +
+    //         "<p><a href='add'>Add another artwork</a> or <a href='artworks'>view the updated list</a> of artworks.</p>" +
+    //         "</body>" +
+    //         "</html>";
+    //     return Content(html, "text/html");
+    // }
+
+    // // GET: /gallery/details/{artworkId}
+    // // Corresponds to http://localhost:50xx/gallery/details/42
+    // [HttpGet]
+    // [Route("/gallery/details/{artworkId}")]
+    // public IActionResult Details(int artworkId)
+    // {
+    //     Console.WriteLine(artworkId);
+    //     string html = "<html>" +
+    //         "<body>" +
+    //         "<h3>Artwork</h3>" +
+    //         "<p><b>ID:</b> " + artworkId + "</p>" +
+    //         "<p><b>Name:</b> " + artworks[artworkId] + "</p>" +
+    //         "</body>" +
+    //         "</html>";
+    //     return Content(html, "text/html");
+    // }
+}
