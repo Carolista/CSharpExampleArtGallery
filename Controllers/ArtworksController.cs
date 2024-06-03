4a78ee78f378ff1030aa9b7552504b6387c0b968
Carolista <cjones.renaissancewoman@gmail.com>
2024-06-02 23:50:45 -0400

    added data layer

--- Controllers/ArtworksController.cs
+++ Controllers/ArtworksController.cs
@@ -6,19 +6,11 @@ namespace CSharpExampleArtGallery;
 public class ArtworksController : Controller
 {
 
-    // TODO 5: Create ArtworksData class to serve as data layer
-
-    // TEST DATA - simulate database
-    private static int nextId = 1;
-    private static readonly Dictionary<int, Artwork> artworks = [];
-
-    // TODO 6: Refactor to retrieve list from ArtworksData
-
     // Endpoint: GET http://localhost:50xx/artworks
     [HttpGet]
     public IActionResult RenderArtworksPage()
     {
-        ViewBag.ArtworkList = artworks.Values; // Loosely typed
+        ViewBag.ArtworkList = ArtworksData.GetAll();
         return View("Index"); // Name of template
     }
 
@@ -29,8 +21,6 @@ public class ArtworksController : Controller
         return View("Add"); // Name of template
     }
 
-    // TODO 6: Refactor to save Artwork object to ArtworksData
-
     // TODO 8: Refactor again for model binding
 
     // Endpoint: POST http://localhost:50xx/artworks/add 
@@ -38,8 +28,7 @@ public class ArtworksController : Controller
     [HttpPost("add")]
     public IActionResult ProcessAddArtworkForm(string title, string artist)
     {
-        artworks.Add(nextId, new Artwork(title, artist));
-        nextId++;
+        ArtworksData.Add(new Artwork(title, artist));
         return Redirect("/artworks"); // Route
     }
 
