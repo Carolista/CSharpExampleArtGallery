d783e0fb582b1e6db5a5acaff508f7f7c5b36056
Carolista <cjones.renaissancewoman@gmail.com>
2024-06-02 23:27:29 -0400

    added TODOs for ch14

--- Controllers/ArtworksController.cs
+++ Controllers/ArtworksController.cs
@@ -5,6 +5,13 @@ namespace CSharpExampleArtGallery;
 [Route("/artworks")]
 public class ArtworksController : Controller
 {
+
+    // TODO 1: Create an Artwork model with properties Id, Title, and Artist
+    
+    // TODO 3: Modify Dictionary to hold Artwork objects instead of strings
+
+    // TODO 5: Create ArtworksData class to serve as data layer
+
     // TEST DATA - simulate database
     private static int nextId = 6;
     private static readonly Dictionary<int, string> artworks = new() {
@@ -14,6 +21,10 @@ public class ArtworksController : Controller
         { 4, "The Persistence of Memory" },
         { 5, "The Starry Night" },
     };
+
+    // TODO 4: Pass list of Artworks objects, not strings
+
+    // TODO 6: Refactor to retrieve list from ArtworksData
 
     // Endpoint: GET http://localhost:50xx/artworks
     [HttpGet]
@@ -30,6 +41,13 @@ public class ArtworksController : Controller
         return View("Add"); // Name of template
     }
 
+    // TODO 3: Modify to accept title and artist,
+    // create Artwork object, and set in dictionary above
+
+    // TODO 6: Refactor to save Artwork object to ArtworksData
+
+    // TODO 8: Refactor again for model binding
+
     // Endpoint: POST http://localhost:50xx/artworks/add 
     // Parameter has same name as incoming form data
     [HttpPost("add")]
@@ -39,4 +57,6 @@ public class ArtworksController : Controller
         nextId++;
         return Redirect("/artworks"); // Route
     }
+
+    // TODO 7: Create Delete template and GET & POST methods
 }
