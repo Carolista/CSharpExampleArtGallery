a9a70ff770d567445c875f931b213ab49862fe32
Carolista <cjones.renaissancewoman@gmail.com>
2024-06-03 20:14:14 -0400

    linted

--- Controllers/ArtworksController.cs
+++ Controllers/ArtworksController.cs
@@ -7,13 +7,15 @@ public class ArtworksController : Controller
 {
     // TEST DATA - simulate database
     private static int nextId = 6;
-    private static readonly Dictionary<int, string> artworks = new() {
-        { 1, "Girl with a Pearl Earring" },
-        { 2, "Mona Lisa" },
-        { 3, "The Birth of Venus" },
-        { 4, "The Persistence of Memory" },
-        { 5, "The Starry Night" },
-    };
+    private static readonly Dictionary<int, string> artworks =
+        new()
+        {
+            { 1, "Girl with a Pearl Earring" },
+            { 2, "Mona Lisa" },
+            { 3, "The Birth of Venus" },
+            { 4, "The Persistence of Memory" },
+            { 5, "The Starry Night" },
+        };
 
     // Endpoint: GET http://localhost:5xxx/artworks
     [HttpGet]
@@ -30,7 +32,7 @@ public class ArtworksController : Controller
         return View("Add"); // Name of template
     }
 
-    // Endpoint: POST http://localhost:5xxx/artworks/add 
+    // Endpoint: POST http://localhost:5xxx/artworks/add
     // Parameter has same name as incoming form data
     [HttpPost("add")]
     public IActionResult ProcessAddArtworkForm(string artwork)
